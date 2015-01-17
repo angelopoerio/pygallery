8661e94f8bf0babe8e215c121e4715ecb35b6ba1
angelopoerio <angelo.poerio@gmail.com>
2015-01-17 16:02:58 +0000

    test

--- pygallery/pygallery/settings.py
+++ pygallery/pygallery/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
@@ -64,6 +64,9 @@ DATABASES = {
 	'PASSWORD': 'intermundia',
     }
 }
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
